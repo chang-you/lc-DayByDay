0b51cbe5cc77f739674a3aa4aee1d721d512dd40
changyou <changyou1009@gmail.com>
2021-11-01 15:08:56 -0700

    346

---
+++ _346_Moving Average from Data Stream.java
@@ -0,0 +1,31 @@
+class MovingAverage {
+    
+     // time : O(n)
+     // space : O(n)
+
+    private Queue<Integer> queue;
+    private double sum = 0;
+    private int size;
+    
+    public MovingAverage(int size) {
+        queue = new LinkedList<>();
+        this.size = size;
+        
+    }
+    
+    public double next(int val) {
+        if (queue.size() == size) {
+            sum -= queue.remove();
+        }
+        sum += val;
+        queue.offer(val);
+        return sum / queue.size();
+        
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
